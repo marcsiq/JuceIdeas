0138153b1d0880c856fc478f83f33cb0a3ff0359
marcsiq <marcsiquierpenyafort@gmail.com>
2020-06-07 13:16:36 +0100

    Added finish window, check for any size.

--- TicTacToe/Source/MainComponent.cpp
+++ TicTacToe/Source/MainComponent.cpp
@@ -9,31 +9,40 @@
 #include "MainComponent.h"
 
 #define WIDTH_HEIGHT 400
-#define GAME_SIZE 3
-#define ELEMENTS_PADDING 10
+#define GAME_SIZE 10
+#define ELEMENTS_PADDING (int)(0.2*WIDTH_HEIGHT/GAME_SIZE)
 
-char board[GAME_SIZE][GAME_SIZE] = { "" };
+char board[GAME_SIZE][GAME_SIZE];
 
 char playerX = 'X';
 char playerO = 'O';
 
 char players[2] = { playerX, playerO};
+int playersWins[2] = { 0,0 };
 
 int currentPlayer = 0;
+int turns;
+
+bool check3(char a, char b, char c)
+{
+    return (a == b && b == c && a!=NULL);
+}
 
 //==============================================================================
 MainComponent::MainComponent()
 {
+    setLookAndFeel(&tttlf);
 
     setSize (WIDTH_HEIGHT, WIDTH_HEIGHT);
-    currentPlayer = players[0];
+       
+    reset();
 
-    addMouseListener(this, false);
-
+    currentPlayer = 0;
 }
 
 MainComponent::~MainComponent()
 {
+    setLookAndFeel(nullptr);
 }
 
 void MainComponent::mouseDown(const MouseEvent& event)
@@ -48,11 +57,33 @@ void MainComponent::mouseDown(const MouseEvent& event)
     {
         board[x][y] = players[currentPlayer];
         currentPlayer = currentPlayer == 0 ? 1 : 0;
-    
+        turns--;
     }
    
     updateSymbols();
-    repaint();
+
+    char winner = checkWinner();
+
+    if (winner != NULL || turns == 0)
+    {
+    
+        String s;
+        if (winner != NULL)
+        {
+            playersWins[winner == playerX ? 0 : 1]++;
+            s << "PLAYER [ " << winner  << " ] WINS\n\n";
+            s << " [ " << playerX << " ] --> " << playersWins[0] << " wins" << "\n";
+            s << " [ " << playerO << " ] --> " << playersWins[1] << " wins";
+        }
+        else
+        {
+            s << "It's a TIE! \n";
+        }
+
+        AlertWindow::showMessageBox(AlertWindow::AlertIconType::NoIcon, "GAME FINISHED", s, "Play Again!", this);
+
+        reset();
+    }
 
 }
 
@@ -63,10 +94,115 @@ void MainComponent::paint (Graphics& g)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
     g.strokePath(grid, PathStrokeType (4.0f));
     g.setColour(Colours::antiquewhite);
-    g.strokePath(symbolsO, PathStrokeType(6.0f));
+    g.strokePath(symbolsO, PathStrokeType(7.0f));
     g.setColour(Colours::indianred);
-    g.strokePath(symbolsX, PathStrokeType(6.0f));
+    g.strokePath(symbolsX, PathStrokeType(7.0f));
+    g.setColour(Colours::green);
+    g.setOpacity(0.7);
+    g.strokePath(winnerp, PathStrokeType(7.0f));
+}
 
+void MainComponent::reset( void )
+{
+    turns = GAME_SIZE * GAME_SIZE;
+
+    for (int i = 0; i < GAME_SIZE; i++)
+    {
+        for (int j = 0; j < GAME_SIZE; j++)
+        {
+            board[i][j] = NULL;
+        }
+    }
+    winnerp.clear();
+    updateSymbols();
+}
+
+char MainComponent::checkWinner(void)
+{
+    for (int i = 0; i < GAME_SIZE; i++)
+    {
+        for (int j = 0; j < GAME_SIZE; j++)
+        {
+            if (j - 1 >= 0 && j + 1 < GAME_SIZE)
+            {
+                if (check3(board[i][j - 1], board[i][j], board[i][j + 1]))
+                {
+                    addWinnerPath(i, j - 1, i, j + 1);
+                    return board[i][j];
+                }
+            }
+            if (i - 1 >= 0 && i + 1 < GAME_SIZE)
+            {
+                if (check3(board[i - 1][j], board[i][j], board[i + 1][j]))
+                {
+                    addWinnerPath(i - 1, j, i + 1, j);
+                    return board[i][j];
+                }
+            }
+            if (i - 1 >= 0 && j - 1 >= 0 && i + 1 < GAME_SIZE && j + 1 < GAME_SIZE)
+            {
+                // First diagonal
+                if (check3(board[i - 1][j - 1], board[i][j], board[i + 1][j + 1]))
+                {
+                    addWinnerPath(i - 1, j - 1, i + 1, j + 1);
+                    return board[i][j];
+                }
+                // Second diagonal
+                if (check3(board[i - 1][j + 1], board[i][j], board[i + 1][j - 1]))
+                {
+                    addWinnerPath(i + 1, j - 1, i - 1, j + 1);
+                    return board[i][j];
+                }
+            }
+        }
+    }
+    return NULL;
+}
+
+
+void MainComponent::addWinnerPath(int si, int sj, int ei, int ej)
+{
+    int w = getWidth() / GAME_SIZE;
+    int h = getHeight() / GAME_SIZE;
+
+    int sx=0, sy=0, ex=0, ey=0;
+
+    if (si == ei)
+    {
+        //vertical
+        ex = sx = si * w + w / 2;
+        sy = sj * h + ELEMENTS_PADDING ;
+        ey = ej * h + h - ELEMENTS_PADDING;
+    }
+    else if (sj == ej)
+    {
+        //horizontal
+        ey = sy = sj * h + h / 2;
+        sx = si * w + ELEMENTS_PADDING;
+        ex = ei * w + w - ELEMENTS_PADDING;
+    }
+    else
+    {
+        //diagonal1
+        if (si < ei)
+        {
+            sx = si * w + ELEMENTS_PADDING;
+            ex = ei * w + w - ELEMENTS_PADDING;
+            sy = sj * h + ELEMENTS_PADDING;
+            ey = ej * h + h - ELEMENTS_PADDING;
+        }
+        else
+        {
+            sx = si * w + w - ELEMENTS_PADDING;
+            ex = ei * w + ELEMENTS_PADDING;
+            sy = sj * h + ELEMENTS_PADDING;
+            ey = ej * h + h - ELEMENTS_PADDING;
+        }
+    }
+
+
+    winnerp.startNewSubPath(sx, sy);
+    winnerp.lineTo(ex, ey);
 }
 
 void MainComponent::updateSymbols(void)
@@ -75,7 +211,7 @@ void MainComponent::updateSymbols(void)
     int h = getHeight() / GAME_SIZE;
 
     symbolsO.clear();
-    symbolsO.clear();
+    symbolsX.clear();
 
     for (int i = 0; i < GAME_SIZE; i++)
     {
@@ -97,6 +233,8 @@ void MainComponent::updateSymbols(void)
             }
         }
     }
+
+    repaint();
 }
 
 void MainComponent::resized()
@@ -106,7 +244,7 @@ void MainComponent::resized()
 
     grid.clear();
 
-    for (int i = 1; i < GAME_SIZE; i++)
+    for (int i = 0; i < GAME_SIZE+1; i++)
     {
         grid.startNewSubPath(i*w / GAME_SIZE, 0);
         grid.lineTo(i*w / GAME_SIZE, w);
--- TicTacToe/Source/MainComponent.h
+++ TicTacToe/Source/MainComponent.h
@@ -10,6 +10,39 @@
 
 #include <JuceHeader.h>
 
+class TicTacToeLF : public LookAndFeel_V4
+{
+public:
+    TicTacToeLF() 
+    {
+        
+    }
+private:
+    //==============================================================================
+    AlertWindow* createAlertWindow(const String& title, const String& message,
+        const String& button1, const String& button2, const String& button3,
+        AlertWindow::AlertIconType iconType,
+        int numButtons, Component* associatedComponent) override
+    {
+        auto* aw = LookAndFeel_V2::createAlertWindow(title, message, button1, button2, button3,
+            iconType, numButtons, associatedComponent);
+
+        auto bounds = aw->getBounds();
+        bounds = bounds.withSizeKeepingCentre(bounds.getWidth() -100, bounds.getHeight());
+        aw->setBounds(bounds);
+
+        aw->setOpaque(false);
+        aw->setColour(AlertWindow::ColourIds::backgroundColourId, findColour(ResizableWindow::backgroundColourId).withAlpha(0.85f));
+
+        for (auto* child : aw->getChildren())
+            if (auto* button = dynamic_cast<TextButton*> (child))
+                button->setBounds(button->getBounds() + Point<int>(-50, -5));
+
+        return aw;
+    }
+};
+
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
@@ -32,9 +65,15 @@ private:
 
     Path grid;
     Path symbolsX, symbolsO;
+    Path winnerp;
 
     void mouseDown(const MouseEvent& event) override;
-    void updateSymbols(void);
+    void updateSymbols(void);    
+    char checkWinner(void);
+    void reset( void );
+    void addWinnerPath(int si, int sj, int ei, int ej);
+
+    TicTacToeLF tttlf;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
